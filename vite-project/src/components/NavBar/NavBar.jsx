a88bf5ad80d864630f6ec399d357e9f646abc672
aasimd <dongarkar21@gmail.com>
2023-11-22 19:19:08 +0530

    final commit

--- vite-project/src/components/NavBar/NavBar.jsx
+++ vite-project/src/components/NavBar/NavBar.jsx
@@ -3,32 +3,41 @@
 import React from "react";
 import { NavLink, useNavigate } from "react-router-dom";
 import { getActiveStyles } from "../../utils";
-
+import "./NavBar.css";
 const NavBar = () => {
 	const navigate = useNavigate();
 	return (
 		<div>
+			{" "}
 			<div>
-				<h2 onClick={() => navigate("/")}>Event Mingle</h2>
+				Source Code:{" "}
+				<a target="_blank" href="https://github.com/aasimd/Event-Mingle">
+					<b title="event-mingle">Github</b>
+				</a>
 			</div>
-			<div>
-				<ul>
-					<li>
-						<NavLink style={getActiveStyles} to={"/"}>
-							Home
-						</NavLink>
-					</li>
-					<li>
-						<NavLink style={getActiveStyles} to={"/volunteers"}>
-							Volunteers
-						</NavLink>
-					</li>
-					<li>
-						<NavLink style={getActiveStyles} to={"/events"}>
-							Events
-						</NavLink>
-					</li>
-				</ul>
+			<div className="nav-bar">
+				<div>
+					<h2 onClick={() => navigate("/")}>Event Mingle</h2>
+				</div>
+				<div>
+					<ul>
+						<li>
+							<NavLink style={getActiveStyles} to={"/"}>
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink style={getActiveStyles} to={"/volunteers"}>
+								Volunteers
+							</NavLink>
+						</li>
+						<li>
+							<NavLink style={getActiveStyles} to={"/events"}>
+								Events
+							</NavLink>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</div>
 	);
